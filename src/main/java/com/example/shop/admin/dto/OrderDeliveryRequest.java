f5136b0dfe72e51185de4442a9f3419072f5a8f6
DongminL <milkskfk12@gmail.com>
2024-12-04 21:27:28 +0900

    fix: redisTemplate 설정 수정

---
+++ src/main/java/com/example/shop/admin/dto/OrderDeliveryRequest.java
@@ -0,0 +1,21 @@
+package com.example.shop.admin.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class OrderDeliveryRequest {
+
+    @Email(message = "이메일 형식이 맞지 않습니다.")
+    @NotBlank(message = "공백일 수 없습니다. 이메일을 입력해주세요.")
+    private String email;
+
+    @NotBlank(message = "공백일 수 없습니다. 주문 번호를 입력해주세요.")
+    private Long orderId;
+}
