7e92cc800d08720d91166f16206dbc6c832417b4
DongminL <milkskfk12@gmail.com>
2024-12-04 20:54:25 +0900

    fix: 비동기 관련 설정 적용되게 수정

--- src/main/java/com/example/shop/global/config/AsyncConfig.java
+++ src/main/java/com/example/shop/global/config/AsyncConfig.java
@@ -1,16 +1,19 @@
 package com.example.shop.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @EnableAsync
 @Configuration
 public class AsyncConfig implements AsyncConfigurer {
 
+    @Bean(name = "customTaskExecutor")
     @Override
     public Executor getAsyncExecutor() {
         int processors = Runtime.getRuntime().availableProcessors();
@@ -18,9 +21,10 @@ public class AsyncConfig implements AsyncConfigurer {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(processors);
         executor.setMaxPoolSize(processors * 2);
-        executor.setQueueCapacity(50);
-        executor.setKeepAliveSeconds(30);
+        executor.setQueueCapacity(1000);
+        executor.setKeepAliveSeconds(60);
         executor.setThreadNamePrefix("AsyncExecutor-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
 
         return executor;
