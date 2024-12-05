fec42bf499efecf06562e2d88efa4098f8474ee0
leeys9423 <leeys9423@gmail.com>
2024-12-05 15:27:06 +0900

    feat: 회원 자신이 주문한 주문목록 조회기능 추가

--- src/main/java/com/example/shop/domain/order/OrderRepository.java
+++ src/main/java/com/example/shop/domain/order/OrderRepository.java
@@ -1,5 +1,8 @@
 package com.example.shop.domain.order;
 
+import com.example.shop.domain.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +12,6 @@ import java.util.Optional;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     Optional<Order> findByOrderNumber(String orderNumber);
-  
+
+    Page<Order> findByUserId(Long userId, Pageable pageable);
 }
--- src/main/java/com/example/shop/user/controller/OrderController.java
+++ src/main/java/com/example/shop/user/controller/OrderController.java
@@ -1,13 +1,18 @@
 package com.example.shop.user.controller;
 
+import com.example.shop.common.dto.PageResponse;
 import com.example.shop.user.dto.CreateOrderRequest;
+import com.example.shop.user.dto.OrderListResponse;
 import com.example.shop.user.dto.OrderResponse;
 import com.example.shop.user.dto.UpdateOrderRequest;
 import com.example.shop.user.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +33,12 @@ public class OrderController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping
+    public ResponseEntity<PageResponse<OrderListResponse>> getOrders(
+            @RequestParam(defaultValue = "1") int page
+            , @RequestParam(defaultValue = "10") int size) {
+        PageResponse<OrderListResponse> response = orderService.getOrders(page, size);
+        return ResponseEntity.ok(response);
+    }
 
 }
