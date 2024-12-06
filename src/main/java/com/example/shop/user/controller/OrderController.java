4d78fd537db7629529d76699e11c4d712b829953
leeys9423 <leeys9423@gmail.com>
2024-12-07 04:40:26 +0900

    docs: 관리자 주문, 사용자 장바구니, 사용자 주문 swagger 관련 어노테이션 추가

--- src/main/java/com/example/shop/user/controller/OrderController.java
+++ src/main/java/com/example/shop/user/controller/OrderController.java
@@ -6,11 +6,14 @@ import com.example.shop.common.dto.OrderListResponse;
 import com.example.shop.common.dto.OrderResponse;
 import com.example.shop.user.dto.UpdateOrderRequest;
 import com.example.shop.user.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "User-Order")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/orders")
@@ -18,16 +21,19 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    @Operation(summary = "주문 생성")
     @PostMapping
     public ResponseEntity<OrderResponse> createOrder(@RequestBody @Valid CreateOrderRequest request) {
         return ResponseEntity.ok(orderService.createOrder(request));
     }
 
+    @Operation(summary = "주문 수정")
     @PutMapping("/{orderNumber}")
     public ResponseEntity<OrderResponse> updateOrder(@PathVariable String orderNumber, @RequestBody @Valid UpdateOrderRequest request) {
         return ResponseEntity.ok(orderService.updateOrder(orderNumber, request));
     }
 
+    @Operation(summary = "주문 목록 조회")
     @GetMapping
     public ResponseEntity<PageResponse<OrderListResponse>> getOrders(
             @RequestParam(defaultValue = "1") int page
@@ -35,11 +41,13 @@ public class OrderController {
         return ResponseEntity.ok(orderService.getOrders(page, size));
     }
 
+    @Operation(summary = "주문 상세 조회")
     @GetMapping("/{orderNumber}")
     public ResponseEntity<OrderResponse> getOrder(@PathVariable String orderNumber) {
         return ResponseEntity.ok(orderService.getOrder(orderNumber));
     }
 
+    @Operation(summary = "주문 취소")
     @PostMapping("/{orderNumber}/cancel")
     public ResponseEntity<OrderResponse> cancelOrder(@PathVariable String orderNumber) {
         return ResponseEntity.ok(orderService.cancelOrder(orderNumber));
