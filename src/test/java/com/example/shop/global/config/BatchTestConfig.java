d055f26808c1625bd3fbfbd68a98b38e13ac2fa8
DongminL <milkskfk12@gmail.com>
2024-12-08 02:28:34 +0900

    fix: spring batch가 h2 db에 적용되도록 설정

--- src/test/java/com/example/shop/global/config/BatchTestConfig.java
+++ src/test/java/com/example/shop/global/config/BatchTestConfig.java
@@ -1,21 +1,11 @@
 package com.example.shop.global.config;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
 
 @TestConfiguration
+@EnableBatchProcessing
+@EnableAutoConfiguration
 public class BatchTestConfig {
-
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder.create()
-                .driverClassName("org.h2.Driver")
-                .url("jdbc:h2:mem:testdb;MODE=MySQL")
-                .username("sa")
-                .password("")
-                .build();
-    }
 }
