8c7e22ba2b653e00cc95c4b4c00223fd1f523e52
DongminL <milkskfk12@gmail.com>
2024-12-07 23:57:01 +0900

    feat: h2 DB를 사용하도록 설정

--- src/test/java/com/example/shop/global/annotation/RepositoryTest.java
+++ src/test/java/com/example/shop/global/annotation/RepositoryTest.java
@@ -1,9 +1,11 @@
 package com.example.shop.global.annotation;
 
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public abstract class RepositoryTest {
 }
--- src/test/java/com/example/shop/global/annotation/ServiceTest.java
+++ src/test/java/com/example/shop/global/annotation/ServiceTest.java
@@ -2,6 +2,7 @@ package com.example.shop.global.annotation;
 
 import com.example.shop.global.config.RedisTestConfig;
 import com.example.shop.global.config.RedisTestContainer;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
@@ -9,5 +10,6 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 @Import(RedisTestConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public abstract class ServiceTest extends RedisTestContainer {
 }
