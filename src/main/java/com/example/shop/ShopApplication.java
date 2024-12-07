87636c059bbc8922078b91d6a897ea6054667609
DongminL <milkskfk12@gmail.com>
2024-12-07 11:53:32 +0900

    refactor: JPA 테스트 시, 오류 예방

--- src/main/java/com/example/shop/ShopApplication.java
+++ src/main/java/com/example/shop/ShopApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @PropertySource("classpath:security.properties")
 public class ShopApplication {
 
