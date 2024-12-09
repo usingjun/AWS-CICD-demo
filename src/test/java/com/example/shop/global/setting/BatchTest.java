5d105c083b17b2bb40751af48dc8bd91455a6833
DongminL <milkskfk12@gmail.com>
2024-12-09 13:05:48 +0900

    test: batch test용 템플릿 클래스

---
+++ src/test/java/com/example/shop/global/setting/BatchTest.java
@@ -0,0 +1,7 @@
+package com.example.shop.global.setting;
+
+import org.springframework.batch.test.context.SpringBatchTest;
+
+@SpringBatchTest
+public abstract class BatchTest extends ServiceTest {
+}
